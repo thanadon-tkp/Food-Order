cccb2be5b0cac78232bc7e29ae2ca5e6e062a400
thanadon-tkp <thanadon.tkp@gmail.com>
2023-03-05 12:24:51 +0700

    Initial Commit

---
+++ Food Ordering/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Food Ordering
+//
+//  Created by Thanadon Trakanpong on 5/3/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food Ordering/Food_OrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_OrderingApp.swift
+//  Food Ordering
+//
+//  Created by Thanadon Trakanpong on 5/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Food_OrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
